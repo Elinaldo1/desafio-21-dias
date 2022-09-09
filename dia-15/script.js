75a3441fc7f2073f00f8d1c612c6350889685adc
elinaldo <elinnaldosc@gmail.com>
2022-09-09 08:55:24 -0300

    add lib scrollReveal

---
+++ dia-15/script.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.containerProduct', {delay: 100});
+ScrollReveal().reveal('.productTitle', {delay: 200});
+ScrollReveal().reveal('.productDescription', {delay: 300});
+ScrollReveal().reveal('.productPrice', {delay: 400});
